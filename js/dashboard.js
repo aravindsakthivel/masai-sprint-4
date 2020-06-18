844afc821b3dcd890ff107ca008aff386d25fe3d
aravindsakthivel <aravind.sakthi@outlook.com>
2020-06-18 12:43:32 +0530

    modified:display of data in table

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,15 +2,17 @@ var data = JSON.parse(localStorage.getItem('loggedUser')) || []
 console.log(data.loggedUser)
 // console.log((Date().split("").slice(11, 15).join("")))
 var dataExpense = []
-var income = 0
+var income = 10000
 var just_check = JSON.parse(localStorage.getItem(data.loggedUser))
 if (just_check !== null){
     console.log(just_check)
-    console.log(just_check[0].expenses[0])
+    console.log(just_check[0].expenses[0].day.length)
 }
 
 
 function pageLoad(){
+    var d_table = document.getElementById('output_table')
+    d_table.style.display = "none"
     var expensesBlock = document.getElementById('expenses_block')
     expensesBlock.style.display = 'none'
     if (data.length !== 0){
@@ -30,14 +32,55 @@ function income_block(){
 function getIncomeData(){
     event.preventDefault()
     var children = event.target.querySelector("#monthly_income").value
-    income = children
+    income = children || 10000
     console.log(income)
 }
 
 
-function progressBarManipulation(){
+function display_table(){
+    var d_table = document.getElementById('output_table')
+    d_table.style.display = ""
+    var d_body = document.getElementById('table_body')
+    d_body.innerHTML = ""
 
+    var transportBill = 0
+    var food_OthersBill = 0
+    var rentBill = 0
+    var medicine_HealthBill = 0
+    var others = 'NONE'
+    for (var i = 0 ; i < just_check.length; i++){
+        var table_row = document.createElement('tr')
+        // console.log(just_check[i])
+        for (var j = 0; j < just_check[just_check.length - 1].expenses[i].day.length; j++){
+            var heading = document.createElement('td')
+            // heading.setAttribute('scope', 'row')
+            heading.innerText = just_check[just_check.length - 1].expenses[i].day[j]
+            table_row.append(heading)
+        }
+        transportBill += just_check[just_check.length - 1].expenses[i].day[1]
+        food_OthersBill += just_check[just_check.length - 1].expenses[i].day[2]
+        rentBill += just_check[just_check.length - 1].expenses[i].day[3]
+        medicine_HealthBill += just_check[just_check.length - 1].expenses[i].day[4]
+        d_body.append(table_row)
+    }
+    d_table.append(d_body)
+    // structuring expenses
+    console.log(transportBill,food_OthersBill, rentBill, medicine_HealthBill)
+    var total_bill = transportBill + food_OthersBill + rentBill + medicine_HealthBill
+    console.log(total_bill)
+    progressBarManipulation(total_bill)
 }
+
+function progressBarManipulation(data){
+    var incBar = document.getElementById('inc')
+    var speBar = document.getElementById('spe')
+    incBar.innerText = income - data
+    speBar.innerText = data
+    var incCal = ((income - data) / 100)
+    incBar.style.width = incCal + "%"
+    speBar.style.width = (100 - incCal)+ "%"
+}
+
 
 
 function displayExpenseBlock(){
